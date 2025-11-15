c72bccbe59d278913f8a0735a930c2328ee5b62a
Kaushik Sarkar Admin <kaushiksarkarstore@gmail.com>
2025-11-15 11:46:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-845692863-f4f54",
+  "appId": "1:337444707598:web:59ef89577918d02a770b5f",
+  "apiKey": "AIzaSyCuPSC7qxucmq2vmIIDf_O09-pMvGuzOuc",
+  "authDomain": "studio-845692863-f4f54.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "337444707598"
+};
