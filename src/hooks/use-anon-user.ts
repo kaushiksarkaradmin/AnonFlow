b8d5b3749b4c207df907231c58d35f7332e43182
Kaushik Sarkar Admin <kaushiksarkarstore@gmail.com>
2025-11-15 11:32:27 +0000

    user's ip address will be encrypted

--- src/hooks/use-anon-user.ts
+++ src/hooks/use-anon-user.ts
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import SHA256 from 'crypto-js/sha256';
 
 const USER_TOKEN_KEY = 'anonflow-user-token';
 
@@ -10,7 +11,10 @@ export function useAnonUser() {
   useEffect(() => {
     let userToken = localStorage.getItem(USER_TOKEN_KEY);
     if (!userToken) {
-      userToken = uuidv4();
+      const newId = uuidv4();
+      // We hash the UUID to create a non-reversible token.
+      // This is more secure than storing the raw UUID.
+      userToken = SHA256(newId).toString();
       localStorage.setItem(USER_TOKEN_KEY, userToken);
     }
     setToken(userToken);
